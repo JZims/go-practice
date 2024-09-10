ead33ec366ebe440292aae8b246c32c7aab5f685
JZims <zimmerman.j@hotmail.com>
2024-09-10 12:26:25 -0400

    huge workaoround but we did it

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,49 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
-const url = "http://services.explorecalifornia.org/json/tours.php"
+const url string = "http://services.explorecalifornia.org/json/packages.php"
 
 func main() {
-	fmt.Println("Network requests")
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	checkError(err)
+
+	req.Header = http.Header{
+		"User-Agent": {"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"},
+		"Accept":     {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+		// "Accept-Encoding":           {"gzip, deflate"},
+		// "Accept-Language":           {"en-US,en;q=0.9"},
+		// "Cache-Control":             {"max-age=0"},
+		// "Connection":                {"keep-alive"},
+		// "Dnt":                       {"1"},
+		// "Host":                      {"services.explorecalifornia.org"},
+		// "Referer":                   {"http://services.explorecalifornia.org/json/"},
+		// "Upgrade-Insecure-Requests": {"1"},
+	}
+
+	resp, err := client.Do(req)
+	checkError(err)
+
+	fmt.Printf("Reponse status code is: %v\n", resp.StatusCode)
+	// fmt.Printf("Request Header is: %v\n", req.Header.Get("Host"))
+	defer resp.Body.Close()
+
+	bytes, err := io.ReadAll(resp.Body)
+	checkError(err)
+
+	content := string(bytes)
+
+	fmt.Print(content)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
